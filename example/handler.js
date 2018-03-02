e8e025a4b0372df4926fa191811b730506b9af0d
David García <davidgf1987@gmail.com>
2018-03-02 15:53:04 +0100

    Add some testing utilities

--- example/handler.js
+++ example/handler.js
@@ -1,11 +1,10 @@
-'use strict';
-
 module.exports.hello = (event, context, callback) => {
   if ((event.queryStringParameters || {}).error) callback(new Error('Oh no!'));
+  console.log('The first version');
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'First version',
+      message: 'The first version'
     })
   };
   return callback(null, response);
