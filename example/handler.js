177bd39ed9402d5bbaca504a4b276441d851575e
David García <davidgf1987@gmail.com>
2019-07-28 23:05:28 +0100

    IoT support (#63)
    * Fix unsecure dependencies
    
    * Replace eslint by standard
    
    * Update serverless version
    
    * Add SQS support to README and provide examples
    
    * Update dependencies
    
    * Add IoT support
    
    * Add integration tests
    
    * Add unit tests
    
    * Add Github templates
    
    * Add husky hooks
    
    * Adjust travis node versions to match Lambda runtimes

--- example/handler.js
+++ example/handler.js
@@ -1,11 +1,11 @@
 module.exports.hello = (event, context, callback) => {
-  if ((event.queryStringParameters || {}).error) callback(new Error('Oh no!'));
-  console.log('The first version');
+  if ((event.queryStringParameters || {}).error) callback(new Error('Oh no!'))
+  console.log('The first version')
   const response = {
     statusCode: 200,
     body: JSON.stringify({
       message: 'The first version'
     })
-  };
-  return callback(null, response);
-};
+  }
+  return callback(null, response)
+}
--- serverless-plugin-canary-deployments.test.js
+++ serverless-plugin-canary-deployments.test.js
@@ -1,47 +1,47 @@
-const fs = require('fs');
-const path = require('path');
-const chai = require('chai');
-const _ = require('lodash/fp');
-const { getInstalledPathSync } = require('get-installed-path');
-const ServerlessCanaryDeployments = require('./serverless-plugin-canary-deployments');
+const fs = require('fs')
+const path = require('path')
+const chai = require('chai')
+const _ = require('lodash/fp')
+const { getInstalledPathSync } = require('get-installed-path')
+const ServerlessCanaryDeployments = require('./serverless-plugin-canary-deployments')
 
-const serverlessPath = getInstalledPathSync('serverless', { local: true });
-const AwsProvider = require(`${serverlessPath}/lib/plugins/aws/provider/awsProvider`);
-const Serverless = require(`${serverlessPath}/lib/Serverless`);
-const { expect } = chai;
-const fixturesPath = path.resolve(__dirname, 'fixtures');
+const serverlessPath = getInstalledPathSync('serverless', { local: true })
+const AwsProvider = require(`${serverlessPath}/lib/plugins/aws/provider/awsProvider`)
+const Serverless = require(`${serverlessPath}/lib/Serverless`)
+const { expect } = chai
+const fixturesPath = path.resolve(__dirname, 'fixtures')
 
 describe('ServerlessCanaryDeployments', () => {
-  const stage = 'dev';
-  const options = { stage };
+  const stage = 'dev'
+  const options = { stage }
 
   describe('addCanaryDeploymentResources', () => {
-    const testCaseFiles = fs.readdirSync(fixturesPath);
-    const getTestCaseName = _.pipe(_.split('.'), _.head);
-    const testCaseFileType = _.pipe(_.split('.'), _.get('[1]'));
+    const testCaseFiles = fs.readdirSync(fixturesPath)
+    const getTestCaseName = _.pipe(_.split('.'), _.head)
+    const testCaseFileType = _.pipe(_.split('.'), _.get('[1]'))
     const testCaseContentsFromFiles = _.reduce((acc, fileName) => {
-      const contents = JSON.parse(fs.readFileSync(path.resolve(fixturesPath, fileName)));
-      return _.set(testCaseFileType(fileName), contents, acc);
-    }, {});
-    const testCaseFilesByName = _.groupBy(getTestCaseName, testCaseFiles);
+      const contents = JSON.parse(fs.readFileSync(path.resolve(fixturesPath, fileName)))
+      return _.set(testCaseFileType(fileName), contents, acc)
+    }, {})
+    const testCaseFilesByName = _.groupBy(getTestCaseName, testCaseFiles)
     this.testCases = _.map(
       (caseName) => {
-        const testCaseContents = testCaseContentsFromFiles(testCaseFilesByName[caseName]);
-        return Object.assign(testCaseContents, {caseName});
+        const testCaseContents = testCaseContentsFromFiles(testCaseFilesByName[caseName])
+        return Object.assign(testCaseContents, { caseName })
       },
       Object.keys(testCaseFilesByName)
-    );
+    )
 
     this.testCases.forEach(({ caseName, input, output, service }) => {
       it(`generates the correct CloudFormation templates: test case ${caseName}`, () => {
-        const serverless = new Serverless(options);
-        Object.assign(serverless.service, service);
-        serverless.service.provider.compiledCloudFormationTemplate = input;
-        serverless.setProvider('aws', new AwsProvider(serverless, options));
-        const plugin = new ServerlessCanaryDeployments(serverless, options);
-        plugin.addCanaryDeploymentResources();
-        expect(serverless.service.provider.compiledCloudFormationTemplate).to.deep.equal(output);
-      });
-    });
-  });
-});
+        const serverless = new Serverless(options)
+        Object.assign(serverless.service, service)
+        serverless.service.provider.compiledCloudFormationTemplate = input
+        serverless.setProvider('aws', new AwsProvider(serverless, options))
+        const plugin = new ServerlessCanaryDeployments(serverless, options)
+        plugin.addCanaryDeploymentResources()
+        expect(serverless.service.provider.compiledCloudFormationTemplate).to.deep.equal(output)
+      })
+    })
+  })
+})
